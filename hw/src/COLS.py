03cf4615120b0358fe9215cde544bafefec3e195
Aniruddha Kulkarni <akulka24@ncsu.edu>
2024-01-15 15:23:46 -0500

    rows and cols python

---
+++ hw/src/COLS.py
@@ -0,0 +1,32 @@
+from num import NUM
+from sym import SYM
+import re
+
+class Cols:
+    def __init__(self, names):
+        self.names = names
+        self.all_columns = []
+        self.x_columns = []
+        self.y_columns = []
+        self.target_class = None
+
+        for index, name in enumerate(names):
+            col = NUM(index, name) if re.match("^[A-Z]", name) else SYM(index, name)
+            self.all_columns.append(col)
+
+            if not name.endswith("X"):
+                if name.endswith("!"):
+                    self.target_class = col
+
+                if re.search("[!+-]$", name):
+                    self.y_columns.append(col)
+                else:
+                    self.x_columns.append(col)
+
+    def add(self, row):
+        for columns in [self.x_columns, self.y_columns]:
+            for col in columns:
+                col.add(row.cells[col.at])
+
+    def __repr__(self):
+        return f"Cols(names={self.names})"
